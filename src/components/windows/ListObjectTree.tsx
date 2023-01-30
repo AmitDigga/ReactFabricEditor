991a3b66504bb734b4dfe27908f413d61942bd11
Amit Digga <diggaamitoo7@gamil.com>
2023-01-30 18:26:59 +0530

    Updated ListObject tree for click action

--- src/components/windows/ListObjectTree.tsx
+++ src/components/windows/ListObjectTree.tsx
@@ -6,19 +6,26 @@ import { FabricContext, EditorObject } from '../../lib/core';
 import { EveryObjectProperty } from '../../lib/properties/EveryObjectProperty';
 
 export function ListObjectTree({ property, context }: { property: EveryObjectProperty; context: FabricContext; }): JSX.Element {
+    const forceUpdate = useForceUpdate();
     const parentObjects = (property.context?.state.editorObjects ?? [])
         .filter(o => o.parent == null);
     return <div>
         <h5>{property.name}</h5>
         <div>
-            {parentObjects.map(p => <DisplayParentEditorObject key={p.id} context={context} object={p} canvas={property.canvas} />)}
+            {parentObjects.map(p =>
+                <DisplayParentEditorObject
+                    onDropAction={() => { forceUpdate() }}
+                    onClickAction={() => { forceUpdate() }}
+                    key={p.id}
+                    context={context}
+                    object={p}
+                    canvas={property.canvas} />)}
         </div>
     </div>;
 }
 
 
-export function DisplayParentEditorObject(props: { object: EditorObject; canvas?: fabric.Canvas; context: FabricContext; }) {
-    const forceUpdate = useForceUpdate();
+export function DisplayParentEditorObject(props: { object: EditorObject; canvas?: fabric.Canvas; context: FabricContext; onDropAction: () => void, onClickAction: () => void }) {
     const { object, canvas } = props;
     function allowDrop(ev: React.DragEvent<HTMLDivElement>) {
         ev.preventDefault();
@@ -28,23 +35,27 @@ export function DisplayParentEditorObject(props: { object: EditorObject; canvas?
         onDropCapture={(e) => {
             const data = e.dataTransfer.getData('text');
             props.context.setParentById(data, object.id);
-            forceUpdate();
         }}
 
         onDragOverCapture={allowDrop}
         onDragStartCapture={(e) => {
             e.dataTransfer.setData('text', object.id);
+            props.onDropAction();
         }}
         style={{
             padding: 5,
         }}
         key={object.name}>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            backgroundColor: canvas?.getActiveObject()?.name === object.id ? 'lightblue' : 'white',
+        }}>
             <div
                 onClick={(e) => {
                     canvas?.setActiveObject(object.fabricObject);
                     canvas?.requestRenderAll();
-                    forceUpdate();
+                    props.onClickAction();
                 }}>
                 {object.name}
             </div>
@@ -54,7 +65,15 @@ export function DisplayParentEditorObject(props: { object: EditorObject; canvas?
 
         </div>
         <div style={{ paddingLeft: 10 }}>
-            {object.children.map(child => <DisplayParentEditorObject key={child.id} context={props.context} object={child} canvas={canvas} />)}
+            {object.children.map(child =>
+                <DisplayParentEditorObject
+                    onClickAction={props.onClickAction}
+                    onDropAction={props.onDropAction}
+                    key={child.id}
+                    context={props.context}
+                    object={child}
+                    canvas={canvas}
+                />)}
         </div>
     </div>;
 }
