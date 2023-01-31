b63622993c26032aa6d52653bc58cf6ed55a7259
Amit Digga <diggaamitoo7@gamil.com>
2023-01-31 17:28:41 +0530

    Fix MoveObject command

--- src/lib/plugins/SelectPlugin.ts
+++ src/lib/plugins/SelectPlugin.ts
@@ -1,13 +1,14 @@
 import { fabric } from 'fabric';
+import { IEvent } from 'fabric/fabric-impl';
 import { Plugin } from '../core/Plugin';
 
 export class SelectPlugin extends Plugin {
     canvas: fabric.Canvas | null = null;
     onInit(canvas: fabric.Canvas): void {
         this.onEvent = this.onEvent.bind(this);
-        this.onMoving = this.onMoving.bind(this);
+        this.onMouseUp = this.onMouseUp.bind(this);
         canvas.selection = this.isSelected();
-        canvas.on('object:moving', this.onMoving);
+        canvas.on('mouse:up', this.onMouseUp);
     }
     onSelected(newState: boolean): void {
         if (this.canvas === null) throw new Error('Canvas is null');
@@ -22,12 +23,13 @@ export class SelectPlugin extends Plugin {
     onEvent(e: fabric.IEvent): void {
     }
 
-    onMoving(e: any) {
+    onMouseUp(e: IEvent) {
+        if (!e.target) return;
         this.context?.fabricCommandManager
             .addCommand({
                 type: 'move-object',
                 data: {
-                    id: e.target.name,
+                    id: e.target.name ?? '',
                     left: e.target.left ?? 0,
                     top: e.target.top ?? 0,
                 }
