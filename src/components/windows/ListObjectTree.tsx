5d51f02f0884fc36d9a0c9b9b3b41969f0ded849
Amit Digga <diggaamitoo7@gamil.com>
2023-02-08 18:22:46 +0530

    Update ListObjectTree to use objectRenderer

--- src/components/windows/ListObjectTree.tsx
+++ src/components/windows/ListObjectTree.tsx
@@ -6,8 +6,7 @@ import { ReactFabricContext } from '../../provider-consumer';
 
 type ListObjectTreeProps = {
     property: EveryObjectProperty;
-    getObjectName: (eo: IEditorObject) => string;
-    onClickAction?: (eo: IEditorObject) => void;
+    renderObject: (eo: IEditorObject) => JSX.Element;
 };
 
 export function ListObjectTree(props: ListObjectTreeProps): JSX.Element {
@@ -20,9 +19,8 @@ export function ListObjectTree(props: ListObjectTreeProps): JSX.Element {
         <div>
             {parentObjects.map(p =>
                 <DisplayParentEditorObject
-                    getObjectName={props.getObjectName}
+                    renderObject={props.renderObject}
                     onDropAction={() => { forceUpdate() }}
-                    onClickAction={props.onClickAction ?? (() => { })}
                     key={p.id}
                     object={p} />)}
         </div>
@@ -31,19 +29,17 @@ export function ListObjectTree(props: ListObjectTreeProps): JSX.Element {
 
 
 type DisplayParentEditorObject = {
-    getObjectName: (eo: IEditorObject) => string;
     object: IEditorObject;
     onDropAction: () => void;
-    onClickAction: (eo: IEditorObject) => void;
+    renderObject: (eo: IEditorObject) => JSX.Element;
 };
 
 export function DisplayParentEditorObject(props: DisplayParentEditorObject) {
-    const { object, getObjectName } = props;
+    const { object } = props;
     const context = useContext(ReactFabricContext);
     function allowDrop(ev: React.DragEvent<HTMLDivElement>) {
         ev.preventDefault();
     }
-    const name = getObjectName(object);
     return <div
         draggable
         onDropCapture={(e) => {
@@ -62,30 +58,12 @@ export function DisplayParentEditorObject(props: DisplayParentEditorObject) {
         style={{
             padding: 5,
         }}
-        key={name}>
-        <div style={{
-            display: 'flex',
-            flexDirection: 'row',
-        }}>
-            <div
-                onClick={(e) => {
-                    props.onClickAction(object);
-                }}>
-                {name}
-            </div>
-            <div onClick={() => {
-                context.commandManager.addCommand({
-                    type: 'remove-object',
-                    data: { id: object.id },
-                })
-            }}>X</div>
-
-        </div>
+        key={object.id}>
+        {props.renderObject(object)}
         <div style={{ paddingLeft: 10 }}>
             {object.children.map(child =>
                 <DisplayParentEditorObject
-                    getObjectName={props.getObjectName}
-                    onClickAction={props.onClickAction}
+                    renderObject={props.renderObject}
                     onDropAction={props.onDropAction}
                     key={child.id}
                     object={child}
